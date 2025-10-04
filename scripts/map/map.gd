db509f9002000e84ee659be6ce428d940d291db0
iocutus <iocutus.gamedev@gmail.com>
2025-10-04 19:52:17 +0200

    add some noise to map generation

--- scripts/map/map.gd
+++ scripts/map/map.gd
@@ -3,6 +3,13 @@ class_name Map extends Node2D
 @onready var ground_layer: TileMapLayer = %GroundLayer
 @onready var obstacles_layer: TileMapLayer = %ObstaclesLayer
 @onready var placed_objects: Node2D = %Objects
+@onready var noise_rect: TextureRect = %NoiseRect
+
+var noise_texture: NoiseTexture2D
+var noise_grid: Dictionary[Vector2i, float]
+
+var min_noise = INF
+var max_noise = -INF
 
 func _ready() -> void:
 	generate()
@@ -13,8 +20,24 @@ func _on_place_obstacle(obstacle: Node2D) -> void:
 	placed_objects.add_child(obstacle)
 
 func generate() -> void:
+	noise_texture = noise_rect.texture
+	
+	# generate noise grid
+	for y in get_viewport_rect().size.y / 16:
+		for x in get_viewport_rect().size.x / 16:
+			var cell: Vector2i = Vector2i(x, y)
+			noise_grid[cell] = noise_texture.noise.get_noise_2d(x, y)
+	
+	# normalise noise grid
+	for cell in noise_grid:
+		var val = absf(noise_grid[cell])
+		if val < min_noise:
+			min_noise = val
+		if val > max_noise:
+			max_noise = val
+	
 	_fill_layer(ground_layer, Tiles.GROUND_0)
-	_place_scattered(obstacles_layer, Tiles.ROCK_SMALL)
+	_place_with_noise(obstacles_layer, Tiles.ROCK_SMALL, Vector2(0.2, 0.3))
 
 func _fill_layer(tile_map_layer: TileMapLayer, tile_id: Vector2i) -> void:
 	for y in get_viewport_rect().size.y / 16:
@@ -22,14 +45,17 @@ func _fill_layer(tile_map_layer: TileMapLayer, tile_id: Vector2i) -> void:
 			var cell: Vector2i = Vector2i(x, y)
 			_set_cell(tile_map_layer, cell, tile_id)
 			
-func _place_scattered(tile_map_layer: TileMapLayer, tile_id: Vector2i) -> void:
+func _place_with_noise(tile_map_layer: TileMapLayer, tile_id: Vector2i, noise_range: Vector2) -> void:
 	for y in get_viewport_rect().size.y / 16:
 		for x in get_viewport_rect().size.x / 16:
-			var rng: float = randf()
-			if rng <= 0.7:
-				continue
 			var cell: Vector2i = Vector2i(x, y)
-			_set_cell(tile_map_layer, cell, tile_id)
+			var raw_noise = absf(noise_grid[cell])
+			var normalized = (raw_noise - min_noise) / (max_noise - min_noise)
+
+			if normalized >= noise_range.x and normalized <= noise_range.y:
+				_set_cell(tile_map_layer, cell, tile_id)
+			else:
+				continue
 
 func _set_cell(layer: TileMapLayer, cell: Vector2i, tile_id: Vector2i) -> bool:
 	if !GridManager.is_cell_free(cell):
