7a1c017e5b09ac2612d9bcc18260e7968b22511f
iocutus <iocutus.gamedev@gmail.com>
2025-10-04 23:25:55 +0200

    adjusted map gen, added temporary radio station scene for testing

--- scripts/map/map.gd
+++ scripts/map/map.gd
@@ -1,5 +1,6 @@
 class_name Map extends Node2D
 
+@export var radio_station_scene: PackedScene
 @onready var ground_layer: TileMapLayer = %GroundLayer
 @onready var obstacles_layer: TileMapLayer = %ObstaclesLayer
 @onready var note_sources_layer: TileMapLayer = %NoteSourcesLayer
@@ -31,23 +32,35 @@ func _on_place_obstacle(obstacle: Node2D) -> void:
 	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(&"generate"):
+		print("SHOULD GENERATE")
+		
 		GridManager.free_grid()
+		print("freed grid")
 		MapManager.free_buildings()
+		print("freed buildings")
 		ground_layer.clear()
+		print("cleared ground layer")
 		obstacles_layer.clear()
+		print("cleared obstacles layer")
 		note_sources_layer.clear()
+		print("cleared note sources layer")
 		for object in placed_objects.get_children():
 			object.queue_free()
-			
+		
+		await get_tree().physics_frame
 		generate()
+		print("called generate again")
 
 func generate() -> void:
+	print("inside generated, called it")
+	
 	noise_texture = noise_rect.texture
 	
 	_generate_noise()
 	
 	_fill_layer(ground_layer, Tiles.SOURCE_0, Tiles.GROUND_0)
-	_place_note_sources(note_sources_layer, Tiles.SOURCE_2, Tiles.NOTE_SOURCE, Vector2i(5, 5), 4)
+	_place_radio_station()
+	_place_note_sources(note_sources_layer, Tiles.SOURCE_2, Tiles.NOTE_SOURCE, Vector2i(6, 6), 3)
 	_place_with_noise(obstacles_layer, Tiles.SOURCE_0, Tiles.ROCK_SMALL, Vector2(0.2, 0.25))
 	_place_patterns(obstacles_layer, tree_pattern_choices, Vector2(0.25, 0.3))
 	
@@ -72,36 +85,57 @@ func _fill_layer(tile_map_layer: TileMapLayer, source_id: int, tile_id: Vector2i
 		for x in get_viewport_rect().size.x / 16:
 			var cell: Vector2i = Vector2i(x, y)
 			_set_cell(tile_map_layer, cell, source_id, tile_id)
-			
-func _place_note_sources(tile_map_layer: TileMapLayer, source_id: int, tile_id: Vector2i, offset: Vector2i, count: int) -> void:
-	var valid_cells: Array[Vector2i] = []
+
+func _place_radio_station() -> void:
+	var radio_station: RadioStation = radio_station_scene.instantiate()
+	placed_objects.add_child(radio_station)
+	
 	var screen_size = get_viewport_rect().size / 16
+	var station_rect = radio_station.tiles.get_used_rect()
 	
-	for y in range(0, screen_size.y):
-		for x in range(0, screen_size.y):
-			var cell: Vector2i = Vector2i(x, y)
-			if GridManager.is_cell_free(cell):
-				valid_cells.append(cell)
+	var spawn_cell = Vector2i(
+		randi_range(0, int(screen_size.x) - station_rect.size.x),
+		randi_range(0, int(screen_size.y) - station_rect.size.y)
+	)
 	
-	valid_cells.shuffle()
+	radio_station.global_position = Vector2(spawn_cell * 16)
 	
-	var placed_cells: Array[Vector2i] = []
-	for candidate in valid_cells:
-		var too_close = false
-		
-		for placed in placed_cells:
-			if abs(candidate.x - placed.x) < offset.x or abs(candidate.y - placed.y) < offset.y:
-				too_close = true
-				break
-		if too_close:
-			continue
-			
-		_set_cell(tile_map_layer, candidate, source_id, tile_id)
-		placed_cells.append(candidate)
-		print("placed cell at: ", candidate)
-		
-		if placed_cells.size() >= count:
+	for cell in radio_station.tiles.get_used_cells():
+		GridManager.set_cell(cell + spawn_cell)
+
+func _place_note_sources(tile_map_layer: TileMapLayer, source_id: int, tile_id: Vector2i, offset: Vector2i, count: int) -> void:
+	var screen_size = get_viewport_rect().size
+	
+	var grid_cols = int(screen_size.x / 16)
+	var grid_rows = int(screen_size.y / 16)
+	
+	var grid_cells: Array[Vector2i] = []
+	for row in range(0, grid_rows):
+		for col in range(0, grid_cols):
+			if row == 0 or col == 0 or row == grid_rows-1 or col == grid_cols-1:
+				continue
+
+			grid_cells.append(Vector2i(col, row))
+	
+	grid_cells.shuffle()
+	
+	var used_rows: Array[int] = []
+	var used_cols: Array[int] = []
+	var placed_count = 0
+	
+	for grid_cell in grid_cells:
+		if placed_count >= count:
 			break
+			
+		var cell = Vector2i(grid_cell.x, grid_cell.y)
+		
+		if cell.y in used_rows or cell.x in used_cols:
+			continue
+		
+		_set_cell(tile_map_layer, cell, source_id, tile_id)
+		used_rows.append(cell.y)
+		used_cols.append(cell.x)
+		placed_count += 1
 			
 func _place_with_noise(tile_map_layer: TileMapLayer, source_id: int, tile_id: Vector2i, noise_range: Vector2) -> void:
 	for y in get_viewport_rect().size.y / 16:
@@ -146,8 +180,14 @@ func _set_pattern(layer: TileMapLayer, cell: Vector2i, source_id: int, pattern_i
 	return true
 	
 func _pattern_fits(cell: Vector2i, pattern: TileMapPattern) -> bool:
+	var screen_size = get_viewport_rect().size / 16
+	
 	for pattern_cell in pattern.get_used_cells():
 		var cell_in_world: Vector2i = cell + pattern_cell
+		
+		if cell_in_world.x < 0 or cell_in_world.y < 0 or cell_in_world.x >= screen_size.x or cell_in_world.y >= screen_size.y:
+			return false
+			
 		if !GridManager.is_cell_free(cell_in_world):
 			return false
 			
---
+++ scripts/radio_station_test.gd
@@ -0,0 +1,6 @@
+class_name RadioStation extends Node2D
+
+@onready var tiles: TileMapLayer = %Tiles
+
+func _ready() -> void:
+	print(tiles)
